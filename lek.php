3889e5b064ee90b7bfcc9714c2a19e3c47a98f8a
zyczynski <32866388+zyczynski@users.noreply.github.com>
2018-08-11 09:07:31 +0200

    klasa lek

---
+++ lek.php
@@ -0,0 +1,40 @@
+<?php
+
+class Lek
+{
+    private $nazwa;
+    private $ilosc;
+    private $zapotrzebowanie;
+
+    function __construct($nazwa, $ilosc, $zapotrzebowanie )
+    {
+        $this->nazwa = $nazwa;
+        $this->ilosc = $ilosc;
+        $this->zapotrzebowanie = $zapotrzebowanie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNazwa()
+    {
+        return $this->nazwa;
+    }
+
+    /**
+     * @param mixed $ilosc
+     */
+    public function setIlosc($ilosc)
+    {
+        $this->ilosc = $ilosc;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getZapotrzebowanie()
+    {
+        return $this->zapotrzebowanie;
+    }
+
+}
