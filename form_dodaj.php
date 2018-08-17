1c8022bd2d395afb6019120909cbe1988cba7b28
zyczynski <32866388+zyczynski@users.noreply.github.com>
2018-08-17 12:43:40 +0200

    usunąłem zmienne sesyjne

--- form_dodaj.php
+++ form_dodaj.php
@@ -10,38 +10,17 @@
 
 <form action="dodaj.php" method="post">
     <label>Nazwa leku <input name="lek" type="text"></label>
-    <?php
-    if (isset($_SESSION['e_lek']))
-    {
-        echo $_SESSION['e_lek'];
-        unset($_SESSION['e_lek']);
-    }
-    ?>
+
 
     <br>
 
     <label>Ilość tabletek <input name="ilosc" type="text"></label>
-
-    <?php
-    if (isset($_SESSION['e_ilosc']))
-    {
-        echo $_SESSION['e_ilosc'];
-        unset($_SESSION['e_ilosc']);
-    }
-    ?>
-
+    
 
     <br>
 
     <label>Zapotrzebowanie dzienne<input name="zapotrzebowanie" type="text"></label>
 
-    <?php
-    if (isset($_SESSION['e_zap']))
-    {
-        echo $_SESSION['e_zap'];
-        unset($_SESSION['e_zap']);
-    }
-    ?>
     <br><br>
     <input type="submit">
 </form>
