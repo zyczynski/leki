92d1eb7a7dd0c41438ec3aecd01548ca8b1c6e31
Bartek <zyczynski7@gmail.com>
2018-11-14 19:26:04 +0100

    wyświetlam tablicę wyników

--- tabela.php
+++ tabela.php
@@ -1,3 +1,9 @@
 <?php
 
 // wyswietl liste wszystkich lekow dodanych do bazy
+
+require_once 'database/database.php';
+
+$wyniki = $pdo->query('SELECT * FROM leki')->fetchAll(PDO::FETCH_ASSOC);
+
+print_r($wyniki);
