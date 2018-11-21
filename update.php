fbf5c59f2aaa959b2a3367b727c34adf1c7c5f28
Bartek <zyczynski7@gmail.com>
2018-11-21 18:11:55 +0100

    Niby powinno być ok..
    ale to co poprzedni, wyświetla echo "zaktualizowano", nie rzuca żadnego błędu, a jednak nie aktualizuje ;/

--- update.php
+++ update.php
@@ -1,11 +1,4 @@
-<?php
-
-require_once 'database/database.php';
-
-$wyniki = $pdo->query('SELECT * FROM leki')->fetchAll(PDO::FETCH_ASSOC);
-
-?>
-
+<?php session_start(); ?>
 <html lang="pl">
 <head>
     <meta charset="UTF-8">
@@ -17,59 +10,29 @@ $wyniki = $pdo->query('SELECT * FROM leki')->fetchAll(PDO::FETCH_ASSOC);
 
     <thead>
     <tr>
-        <th>ID</th>
-        <th>Nazwa</th>
-        <th>Nowa nazwa</th>
-        <th>Ilość</th>
+
+        <th>Nowa Nazwa</th>
         <th>Aktualizuj ilość</th>
-        <th>Zapotrzebowanie</th>
         <th>Aktualizuj zapotrzebowanie</th>
-        <th>Data dodania</th>
     </tr>
     </thead>
 
     <tbody>
-    <?php
 
-    foreach ($wyniki as $wynik)
-    {
-        echo "<tr><td>{$wynik['id']}</td>
-              <td>{$wynik['Nazwa']}</td>
-              <form action='' method='get'>
-                <td><input name=\"nazwa\" type=\"text\"</input></td>
-              <td>{$wynik['Ilosc']}</td>
-                 <td><input name=\"ilosc\" type=\"text\"</input></td>
-              <td>{$wynik['Zapotrzebowanie']}</td>
-                <td><input name=\"zapotrzebowanie\" type=\"text\"</input></td>
-              <td>{$wynik['Data']}</td>
-              </tr>";
+            <tr>
 
-    }
+              <form action='after_update.php' method='post'>
+                <td><input name="nazwa" type="text"</input></td>
+                 <td><input name="ilosc" type="text"</input></td>
+                <td><input name="zapotrzebowanie" type="text"</input></td>
 
-    ?>
+              </tr>
+
+
     </tbody>
 </table>
 <br><br>
 <input type="submit">
 </form>
-
-
-<?php
-
-if ($_GET)
-{
- require_once 'database/database.php';
-
-    $sql = "UPDATE leki SET Nazwa =?, Ilosc = ?, Zapotrzebowanie = ? WHERE id = ?";
-    $pdo->prepare($sql)->execute([$_GET['nazwa'], $_GET['ilosc'], $_GET['zapotrzebowanie']]);
-
-    echo "Zaktualizowano";
-  }
-  else
-  {
-      echo "nie udało się wykonać aktualizacji";
-  }
-
-?>
 </body>
 </html>
