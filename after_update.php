30c3accf549b0092f3f5305db99aa86612bf9b51
bwl <zyczynski7@gmail.com>
2019-01-07 20:40:29 +0100

    ogarłem o.O

--- after_update.php
+++ after_update.php
@@ -4,8 +4,8 @@ if (isset($_POST['ilosc']))
 
     require_once 'database/database.php';
 
-    $sql = "UPDATE leki SET Nazwa = ?, Ilosc = ?, Zapotrzebowanie = ? WHERE id = ?";
-    $pdo->prepare($sql)->execute([$_POST['nazwa'], (Ilosc + $_POST['ilosc']), $_POST['zapotrzebowanie'], $_GET['id']]);
+    $sql = "UPDATE leki SET Nazwa = ?, Ilosc = Ilosc + ?, Zapotrzebowanie = ? WHERE id = ?";
+    $pdo->prepare($sql)->execute([$_POST['nazwa'], $_POST['ilosc'], $_POST['zapotrzebowanie'], $_GET['id']]);
 
     echo "Zaktualizowano";
 }
