f6786b65d471b848d18389dbac64f2b2794ee8ac
bwl <zyczynski7@gmail.com>
2019-01-10 12:34:56 +0100

    poprawione

--- calculate.php
+++ calculate.php
@@ -7,12 +7,12 @@ $wyniki = $pdo->query('SELECT * FROM leki')->fetchAll(PDO::FETCH_ASSOC);
 foreach ($wyniki as $wynik)
 
     {
-        $dzisiejsza_data = date("Y-m-d");
-        $data_dodania = $wynik['Data'];
+
+
         $zapotrzebowanie = $wynik['Zapotrzebowanie'];
 
-        $datetime1 = new DateTime($data_dodania);
-        $datetime2 = new DateTime($dzisiejsza_data);
+        $datetime1 = new DateTime($wynik['Data']);
+        $datetime2 = new DateTime(date("Y-m-d"));
         $interval = $datetime1->diff($datetime2);
         $pozostalo = ($interval->format('%R%a')) * ($zapotrzebowanie);
 
